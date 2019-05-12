a5973340d85566d3ba6aef3c03a2caca3ae4d358
Chae <chaeakf12@gmail.com>
2019-05-12 10:43:06 +0900

    JSON 파싱 기쵸 => 토스트로 화면에 출력

--- app/src/main/java/com/tj/banklistfromserver/MainActivity.java
+++ app/src/main/java/com/tj/banklistfromserver/MainActivity.java
@@ -4,9 +4,13 @@ import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.tj.banklistfromserver.Utils.ConnectServer;
 import com.tj.banklistfromserver.databinding.ActivityMainBinding;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends BaseActivity {
 
@@ -27,8 +31,55 @@ public class MainActivity extends BaseActivity {
         act.serverTestBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ConnectServer.getRequestInfoBank(mContext, null);
+                ConnectServer.getRequestInfoBank(mContext, new ConnectServer.JsonResponseHandler() {
+
+//                    실제 서버에서 돌아온 응답을
+//                    메인엑티비티에서 처리하는 메쏘드
+                    @Override
+                    public void onResponse(JSONObject json) {
+                        try {
+                            int code =json.getInt("code");
+
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+
+                                    if(code == 200){
+
+                                        Toast.makeText(mContext, "정상적으로 데이터를 가지고 왔습니다 ", Toast.LENGTH_SHORT).show();
+
+                                    }else {
+
+//                                        서버에서 주는 에러메세지를 토스트로 출력
+                                        try {
+
+                                            String message = json.getString("message");
+                                            Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
+                                        } catch (JSONException e) {
+
+                                            e.printStackTrace();
+                                        }
+
+                                        Toast.makeText(mContext, "서버통신에 문제가 있습니다 ", Toast.LENGTH_SHORT).show();
+
+                                    }
+
+                                }
+                            });
+
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+
+
+
             }
+
+
         });
 
     }
