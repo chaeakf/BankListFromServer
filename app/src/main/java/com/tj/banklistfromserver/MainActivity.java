e9d99a4720921a48a8f25e0e09051a2c0fcc45ca
Chae <chaeakf12@gmail.com>
2019-05-12 11:51:31 +0900

    자동 새로고침 - 안드로이드 생명주기  - onResum 활용 테스트   - list 클리어 기능 (중복 데이터 제거)

--- app/src/main/java/com/tj/banklistfromserver/MainActivity.java
+++ app/src/main/java/com/tj/banklistfromserver/MainActivity.java
@@ -33,6 +33,7 @@ public class MainActivity extends BaseActivity {
         bindViews();
         setupEvents();
         setValues();
+        onResume();
     }
 
 
@@ -41,73 +42,6 @@ public class MainActivity extends BaseActivity {
         act.serverTestBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ConnectServer.getRequestInfoBank(mContext, new ConnectServer.JsonResponseHandler() {
-
-//                    실제 서버에서 돌아온 응답을
-//                    메인엑티비티에서 처리하는 메쏘드
-                    @Override
-                    public void onResponse(JSONObject json) {
-                        try {
-                            int code =json.getInt("code");
-
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-
-                                    if(code == 200){
-
-                                        Toast.makeText(mContext, "정상적으로 데이터를 가지고 왔습니다 ", Toast.LENGTH_SHORT).show();
-
-                                        try {
-                                            JSONObject data = json.getJSONObject("data");
-                                            JSONArray banks = data.getJSONArray("banks");
-
-                                            for (int i = 0; i < banks.length(); i++){
-                                                JSONObject bank = banks.getJSONObject(i);
-
-//                                                String name = bank.getString("name");
-//                                                Log.d("은행이름", name);
-
-                                                Banks bankObj = Banks.getBankFromJson(bank);
-
-                                                banksList.add(bankObj);
-                                            }
-
-                                            bankListadapter.notifyDataSetChanged();
-
-                                        } catch (JSONException e) {
-
-                                            e.printStackTrace();
-                                        }
-
-                                    }else {
-
-//                                        서버에서 주는 에러메세지를 토스트로 출력
-                                        try {
-
-                                            String message = json.getString("message");
-                                            Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
-                                        } catch (JSONException e) {
-
-                                            e.printStackTrace();
-                                        }
-
-                                        Toast.makeText(mContext, "서버통신에 문제가 있습니다 ", Toast.LENGTH_SHORT).show();
-
-                                    }
-
-                                }
-                            });
-
-
-
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                });
-
-
 
             }
 
@@ -120,6 +54,78 @@ public class MainActivity extends BaseActivity {
     public void setValues() {
         bankListadapter = new BankListadapter(mContext, banksList);
         act.bankListView.setAdapter(bankListadapter);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        ConnectServer.getRequestInfoBank(mContext, new ConnectServer.JsonResponseHandler() {
+
+            //                    실제 서버에서 돌아온 응답을
+//                    메인엑티비티에서 처리하는 메쏘드
+            @Override
+            public void onResponse(JSONObject json) {
+                try {
+                    int code =json.getInt("code");
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            if(code == 200){
+
+                                Toast.makeText(mContext, "정상적으로 데이터를 가지고 왔습니다 ", Toast.LENGTH_SHORT).show();
+
+                                try {
+                                    JSONObject data = json.getJSONObject("data");
+                                    JSONArray banks = data.getJSONArray("banks");
+
+//                                    누적으로 데이터가 쌓이지 않게, 미리 한번 비워주는 코드
+                                    banksList.clear();
+
+                                    for (int i = 0; i < banks.length(); i++){
+                                        JSONObject bank = banks.getJSONObject(i);
+
+//                                                String name = bank.getString("name");
+//                                                Log.d("은행이름", name);
+
+                                        Banks bankObj = Banks.getBankFromJson(bank);
+
+                                        banksList.add(bankObj);
+                                    }
+
+                                    bankListadapter.notifyDataSetChanged();
+
+                                } catch (JSONException e) {
+
+                                    e.printStackTrace();
+                                }
+
+                            }else {
+
+//                                        서버에서 주는 에러메세지를 토스트로 출력
+                                try {
+
+                                    String message = json.getString("message");
+                                    Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
+                                } catch (JSONException e) {
+
+                                    e.printStackTrace();
+                                }
+
+                                Toast.makeText(mContext, "서버통신에 문제가 있습니다 ", Toast.LENGTH_SHORT).show();
+
+                            }
+
+                        }
+                    });
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
     }
 
